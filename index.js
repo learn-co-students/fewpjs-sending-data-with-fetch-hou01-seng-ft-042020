e4e52cb92b9659007a7c0f8dafaa359ec7de7d1c
teeman60 <taiye41@gmail.com>
2020-06-05 00:50:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Add your code here
+// // Add your code here
+// function submitData(name, email) {
+//     fetch('http://localhost:3000/users', 
+//     {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Accept': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             name,
+//             email
+//         })
+        
+//     }) 
+    
+//         .then(function(response) {
+//             return response.json()
+//         })
+//         .then( function ( object ) {
+//             document.body.innerHTML = object[ "id" ]
+//         })
+//         .catch( function ( error ) {
+//             document.body.innerHTML = error.message
+//           } )
+// }
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( json ) {
+        document.body.innerHTML = json[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }

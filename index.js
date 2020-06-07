2a484aa85d977f561b6799ee0b03e0e83b4154e4
github username <github email address>
2020-06-07 13:44:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Add your code here
+
+
+   
+// const url = 'http://localhost:3000/users'
+// function submitData(name,email) {
+//     fetch(url, {
+//         method: "POST",
+//         headers: {
+//             "Content-type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify({
+//             name: name,
+//              email: email
+//          })
+//     })
+//     .then(function(response) {
+//         return response.json();
+//       })
+//       .then(function(object) {
+//         document.body.innerHTML = object["id"]
+//       });
+//       .catch( function ( error ) {
+//         document.body.innerHTML = error.message
+//       } )
+//   }
+
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
+    
